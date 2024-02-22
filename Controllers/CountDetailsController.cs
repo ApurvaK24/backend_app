68bc23ddf30580e66a85d485ae0769fd849e2ec8
ApurvaK <kulkarniapurva2003@gmail.com>
2024-02-22 21:40:26 +0530

    updated

---
+++ Controllers/CountDetailsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net.Mail;
+using Newtonsoft.Json;
+using backend_app.Models;
+
+namespace backend_app.Controllers
+{
+    public class CountDetailsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/CountDetails")]
+        public IHttpActionResult GetCountDetails()
+        {
+            List<CountDetail> countDetails = new List<CountDetail>();
+
+            try
+            {
+                 // Replace with your SQL Server connection string
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("sp_getCountDetails", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                CountDetail detail = new CountDetail();
+                                detail.MonthName = reader["MonthName"].ToString();
+                                detail.IncidentCount = DBNull.Value.Equals(reader["IncidentCount"]) ? 0 : Convert.ToInt32(reader["IncidentCount"]);
+                                detail.AccidentCount = DBNull.Value.Equals(reader["AccidentCount"]) ? 0 : Convert.ToInt32(reader["AccidentCount"]);
+                                countDetails.Add(detail);
+                            }
+                        }
+                    }
+                }
+
+                return Ok(countDetails);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
---
+++ Models/CountDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend_app.Models
+{
+    public class CountDetail
+    {
+        public string MonthName { get; set; }
+        public int IncidentCount { get; set; }
+        public int AccidentCount { get; set; }
+    }
+}
