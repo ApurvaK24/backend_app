9be7981f9fab0bb177fab6af5bd90261cf4cee51
ApurvaK <kulkarniapurva2003@gmail.com>
2024-02-27 23:24:23 +0530

    final commit

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -34,20 +34,37 @@ namespace backend_app.Controllers
                 cmd = new SqlCommand("usp_Registration", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@Name", employee.Name);
-                cmd.Parameters.AddWithValue("@EmployeeId", employee.EmployeeId); // Updated from PhoneNo
+                cmd.Parameters.AddWithValue("@Email", employee.Email); // Updated from PhoneNo
                 cmd.Parameters.AddWithValue("@Password", employee.Password); // Updated from Address
                 cmd.Parameters.AddWithValue("@ReenteredPassword", employee.ReenteredPassword);
                 cmd.Parameters.AddWithValue("@SelectedOption", employee.SelectedOption);
+                /*cmd = new SqlCommand("usp_Registration", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@EmployeeId", employee.EmployeeId); // Updated from PhoneNo
+                cmd.Parameters.AddWithValue("@Password", employee.Password); // Updated from Address
+                cmd.Parameters.AddWithValue("@ReenteredPassword", employee.ReenteredPassword);
+                cmd.Parameters.AddWithValue("@SelectedOption", employee.SelectedOption);*/
                 // cmd.Parameters.AddWithValue("@IsActive", employee.IsActive);
 
                 conn.Open();
                 int i = cmd.ExecuteNonQuery();
                 conn.Close();
 
-                if (i > 0)
+                /*if (i > 0)
                 {
                     // Send email after successful registration
-                    SendEmail(employee);
+                    SendEmail(employee.Email, employee);
+                   
+                    msg = "Data Inserted.";
+                }*/
+                if (i > 0)
+                {
+                    // Send email to the registered user
+                    SendEmail(employee.Email, employee);
+
+                    // Send email to ishakandhare2003@gmail.com
+                    SendEmail("ishakandhare2003@gmail.com", employee);
 
                     msg = "Data Inserted.";
                 }
@@ -64,19 +81,18 @@ namespace backend_app.Controllers
             return msg;
         }
 
-        private void SendEmail(Employee employee)
+        private void SendEmail(string recipientEmail, Employee employee)
         {
-            string toEmail = "ishakandhare2003@gmail.com";
             string subject = "New Registration";
-            string body = $"Name: {employee.Name}\nEmployee Id: {employee.EmployeeId}\nPassword: {employee.Password}\nRe-entered Password: {employee.ReenteredPassword}\nSelected Option: {employee.SelectedOption}";
+            string body = $"Name: {employee.Name}\nEmail: {employee.Email}\nPassword: {employee.Password}\nSelected Option: {employee.SelectedOption}";
 
             try
             {
-                using (var mailMessage = new MailMessage("ishakandhare2003@gmail.com", toEmail))
+                using (var mailMessage = new MailMessage("ishakandhare2003@gmail.com", recipientEmail)) // Send email to both ishakandhare2003@gmail.com and the registered user's email
                 using (var smtpClient = new SmtpClient("smtp.gmail.com"))
                 {
                     smtpClient.Port = 587;
-                    smtpClient.Credentials = new System.Net.NetworkCredential("ishakandhare2003@gmail.com", "acpz sxau yhwa gnae");
+                    smtpClient.Credentials = new System.Net.NetworkCredential("ishakandhare2003@gmail.com", "nfne uroi vonq qczw");
                     smtpClient.EnableSsl = true;
 
                     mailMessage.Subject = subject;
@@ -89,7 +105,7 @@ namespace backend_app.Controllers
             {
                 Console.WriteLine($"Error sending email: {ex.Message}");
                 // Log the exception or handle it appropriately
-            }
+            }
         }
 
 
@@ -101,8 +117,12 @@ namespace backend_app.Controllers
             {
                 using (SqlCommand cmd = new SqlCommand("usp_Login", conn))
                 {
-                    cmd.CommandType = CommandType.StoredProcedure;
+                    /*cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.AddWithValue("@EmployeeId", employee.EmployeeId);
+                    cmd.Parameters.AddWithValue("@Password", employee.Password);
+                    cmd.Parameters.AddWithValue("@SelectedOption", employee.SelectedOption);*/
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Email", employee.Email); // Updated from EmployeeId
                     cmd.Parameters.AddWithValue("@Password", employee.Password);
                     cmd.Parameters.AddWithValue("@SelectedOption", employee.SelectedOption);
 
@@ -114,13 +134,20 @@ namespace backend_app.Controllers
                         {
                             // User is valid, return user data
                             reader.Read();
+                            /* Employee loggedInUser = new Employee
+                             {
+                                 Id = reader.GetInt32(reader.GetOrdinal("ID")),
+                                 Name = reader.GetString(reader.GetOrdinal("NAME")),
+                                 EmployeeId = reader.GetString(reader.GetOrdinal("EmployeeId")),
+                                 // Include other fields as needed
+                             };*/
                             Employee loggedInUser = new Employee
                             {
                                 Id = reader.GetInt32(reader.GetOrdinal("ID")),
                                 Name = reader.GetString(reader.GetOrdinal("NAME")),
-                                EmployeeId = reader.GetString(reader.GetOrdinal("EmployeeId")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")), // Updated from EmployeeId
                                 // Include other fields as needed
-                            };
+                            };
 
                             return Ok(loggedInUser);
                         }
@@ -180,6 +207,8 @@ namespace backend_app.Controllers
 
             return hodCnt;
         }
+
+        
 
 
 
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -8,11 +8,19 @@ namespace backend_app.Models
 {
     public class Employee
     {
-        public int Id { get; set; }
+        /*public int Id { get; set; }
         public string Name { get; set; }
         public string EmployeeId { get; set; } // Updated from PhoneNo
         public string Password { get; set; } // Updated from Address
         public string ReenteredPassword { get; set; }
+        public string SelectedOption { get; set; }*/
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; } // Updated from PhoneNo
+        public string Password { get; set; } // Updated from Address
+        public string ReenteredPassword { get; set; }
         public string SelectedOption { get; set; }
+
     }
 }
